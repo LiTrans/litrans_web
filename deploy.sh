8c12abc3eb267e6deb7d11a9db4c2f5a7245a2e9
Melvin Wong <melvin.wong@ryerson.ca>
2018-12-16 02:47:29 -0500

    rebuilding site Sun, Dec 16, 2018  2:47:29 AM

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+# add the config files
+git add config.toml deploy.sh
+
 # Go To Public folder
 cd docs
 # Add changes to git.
