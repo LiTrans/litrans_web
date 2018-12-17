79b5620ffe59e1fd307ab9d99ac2e9176222b820
Melvin Wong <melvin.wong@ryerson.ca>
2018-12-17 00:30:11 -0500

    rebuilding hugo backend Mon, Dec 17, 2018 12:30:11 AM

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,6 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Pull changes from remote
-git submodule update && git pull
-
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
